4ebd1deff7d69f53d1df01acb06cf3eec60f63ec
Kemal Acar <kemallacar@gmail.com>
2024-02-05 21:31:51 +0100

    Lombok annotations are added.

--- src/main/java/org/ecommerce/app/dto/CheckoutRequestDto.java
+++ src/main/java/org/ecommerce/app/dto/CheckoutRequestDto.java
@@ -1,6 +1,10 @@
 package org.ecommerce.app.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -8,7 +12,12 @@ import java.util.List;
  * @author Kemal Acar
  */
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CheckoutRequestDto {
+
+    @NotEmpty
     private List<String> productIdList;
 }
 
